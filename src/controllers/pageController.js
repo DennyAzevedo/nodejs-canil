a7e478ffd553c9928f3bf25c81414296d930248f
Denny Paulista Azevedo Filho <denny.filho@animaeducacao.com.br>
2022-07-13 18:38:08 -0300

    Ajustando o Menu e os views

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -1,19 +1,53 @@
 const express = require('express');
+const { createMenuObject } = require('../helpers/createMenuObject');
+const { pet } = require('../model/pet')
 
 const home = (req, res) => {
-	res.render('pages/page')
+	let list = pet.getAll();
+	res.render('pages/page', {
+		menu: createMenuObject('all'),
+		banner: {
+			title: 'Todos os animais',
+			background: 'allanimals.jpg'
+		},
+		list
+	})
 }
 
 const dogs = (req, res) => {
-	//res.render('pages/page')
+	let list = pet.getFromType('dog');
+	res.render('pages/page', {
+		menu: createMenuObject('dog'),
+		banner: {
+			title: 'Cachorros',
+			background: 'banner_dog.jpg'
+		},
+		list
+	})
 }
 
 const cats = (req, res) => {
-	//res.render('pages/page')
+	let list = pet.getFromType('cat');
+	res.render('pages/page', {
+		menu: createMenuObject('cat'),
+		banner: {
+			title: 'Gatos',
+			background: 'banner_cat.jpg'
+		},
+		list
+	})
 }
 
 const fishes = (req, res) => {
-	//res.render('pages/page')
+	let list = pet.getFromType('fish');
+	res.render('pages/page', {
+		menu: createMenuObject('fish'),
+		banner: {
+			title: 'Peixes',
+			background: 'banner_fish.jpg'
+		},
+		list
+	})
 }
 
 module.exports.home = home;
